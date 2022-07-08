b3fa243e53addd9a6dee6e19afed5d5ea7174049
Charles Reverand <20057606+taliyos@users.noreply.github.com>
2022-07-08 13:01:28 -0400

    Adjusted to support timeframes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,20 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:3001";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://192.168.1.4:3001";
 
 // Sends a GET request to the backend-server for the latest calendar
 export async function getCalendar() {
+    let startDate = new Date();
+    startDate.setDate(startDate.getDate() - 7);
+    let endDate = new Date();
+    endDate.setDate(endDate.getDate() + 14);
     const response = await fetch(API_BASE_URL + "/calendar", {
-        method: "GET",
+        method: "POST",
         headers: {
-            "Content-Type": "application/json"
-        }
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify ({
+            "startDate": startDate.toUTCString(),
+            "endDate": endDate.toUTCString()
+        })
     });
 
     const data = await response.json();
