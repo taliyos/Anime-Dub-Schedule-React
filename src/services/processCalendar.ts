f60f02e7127eb4b6c36b32d8f741461b271bcef9
Charles Reverand <20057606+taliyos@users.noreply.github.com>
2022-07-26 16:56:06 -0400

    Fixed month change bug
    Fixed bug where a new month would be registered as a previous day and cause all entries to show up under the same date.

--- src/services/processCalendar.ts
+++ src/services/processCalendar.ts
@@ -4,23 +4,32 @@ import { ShowDate } from "../interfaces/showDate";
 
 export function processCalendar(calendar: CalendarItem[]) {
     let showDateCalendar: ShowDate[] = [];
+    
+    let lastNewDay = -1;
+    
 
     for (let i = 0; i < calendar.length; i++) {
         let calDate = new Date(Date.UTC(calendar[i].time.year, calendar[i].time.month, calendar[i].time.day, calendar[i].time.hour, calendar[i].time.minute));
         calendar[i].time.hour = calDate.getHours();
         calendar[i].time.minute = calDate.getMinutes();
-        if (showDateCalendar.length === 0 || (
-            calDate.getFullYear() !== showDateCalendar[showDateCalendar.length - 1].year
-            || calDate.getMonth() !== showDateCalendar[showDateCalendar.length - 1].month
-            || calDate.getDate() !== showDateCalendar[showDateCalendar.length - 1].day
+        if (showDateCalendar.length === 0 || lastNewDay === -1 || (
+            calDate.getFullYear() !== showDateCalendar[lastNewDay].year
+            || calDate.getMonth() !== showDateCalendar[lastNewDay].month
+            || calDate.getDate() !== showDateCalendar[lastNewDay].day
         )) {
+            console.log("new date?");
 
             // Check for a weird time issue
-            if (showDateCalendar.length !== 0 && (calDate.getFullYear() < showDateCalendar[showDateCalendar.length - 1].year
-                || calDate.getMonth() < showDateCalendar[showDateCalendar.length - 1].month
-                || calDate.getDate() < showDateCalendar[showDateCalendar.length - 1].day)) {
+            if (showDateCalendar.length !== 0 && ((calDate.getFullYear() < showDateCalendar[lastNewDay].year
+                                                                            && calDate.getMonth() > showDateCalendar[lastNewDay].month
+                                                                            && calDate.getDate() > showDateCalendar[lastNewDay].day)
+                || (calDate.getMonth() < showDateCalendar[lastNewDay].month && calDate.getDate() > showDateCalendar[lastNewDay].day
+                                                                            && calDate.getFullYear() === showDateCalendar[lastNewDay].year)
+                || (calDate.getDate() < showDateCalendar[lastNewDay].day && calDate.getMonth() === showDateCalendar[lastNewDay].month
+                                                                         && calDate.getFullYear() === showDateCalendar[lastNewDay].year))) {
+                    console.log("not new");
                     // Skip if the date the show belongs to is out of range
-                    if (showDateCalendar.length < 1) continue;
+                    if (showDateCalendar.length < 2) continue;
                     // Add the show to the previous day
                     showDateCalendar[showDateCalendar.length - 2].calendarItems.push(calendar[i]);
                     continue;
@@ -34,6 +43,8 @@ export function processCalendar(calendar: CalendarItem[]) {
                 dayOfWeek: days[calDate.getDay()],
                 calendarItems: [calendar[i]],
             });
+            console.log("new at " + (showDateCalendar.length - 1));
+            lastNewDay = showDateCalendar.length - 1;
         }
         else {
             // Add the calendar item to the last show date
